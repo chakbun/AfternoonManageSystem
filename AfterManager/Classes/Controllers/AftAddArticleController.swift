bf2c4b62b26b5ad235dc78df1570c29d358a3279
chakbun <cheungjaben@hotmail.com>
2015-11-18 16:21:37 +0800

    add article detail controller

---
+++ AfterManager/Classes/Controllers/AftAddArticleController.swift
@@ -0,0 +1,26 @@
+//
+//  AftAddArticleController.swift
+//  AfterManager
+//
+//  Created by Jaben on 15/11/17.
+//  Copyright © 2015年 After. All rights reserved.
+//
+
+import UIKit
+
+class AftAddArticleController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "添加文章"
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+}
